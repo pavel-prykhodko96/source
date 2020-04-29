692d7401e38dcbbeabb8a553093d3104f6c7807c
pavel-prykhodko96 <47929311+pavel-prykhodko96@users.noreply.github.com>
2020-04-29 22:48:24 +0300

    passed 7 - 8 lessons and labs from 3 Unit of App Development with Swift

---
+++ Swift/Lab - About Me/Lab - About Me/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab - About Me
+//
+//  Created by Pavel Prykhodko on 28.04.2020.
+//  Copyright © 2020 Pavel Prykhodko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
