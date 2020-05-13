90b55a5ced0af9855ff5e23fa032a613ecd5955c
pavel-prykhodko96 <47929311+pavel-prykhodko96@users.noreply.github.com>
2020-05-13 23:05:34 +0300

    finished Chapter 4.4 - Scroll Views from App Development with Swift

---
+++ Swift/4.4 Lab - ISpy/4.4 Lab - ISpy/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  4.4 Lab - ISpy
+//
+//  Created by Pavel Prykhodko on 13.05.2020.
+//  Copyright © 2020 Pavel Prykhodko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.delegate = self
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        updateZoomFor(size: view.bounds.size)
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func updateZoomFor(size: CGSize) {
+        let widthScale = size.width / imageView.bounds.width
+        let heightScale = size.height / imageView.bounds.height
+        let scale = min(widthScale, heightScale)
+        scrollView.minimumZoomScale = scale
+        scrollView.zoomScale = scale
+    }
+
+}
+
